bfed97d79e51c8728292cee1262546d5e7c44a4f
“armando3069” <jorapula2@gmail.com>
2024-08-15 03:13:42 +0300

    style:remove space

--- clients/src/components/pizzaCategory/pizza.jsx
+++ clients/src/components/pizzaCategory/pizza.jsx
@@ -2,56 +2,53 @@ import React, { useState, useEffect, useContext } from "react";
 import { ShopContext } from "../../context/shop-context";
 import MenuItems from "../../components/menuItems";
 import { MdOutlineSubdirectoryArrowRight } from "react-icons/md";
-import { Link } from 'react-router-dom';
-
+import { Link } from "react-router-dom";
 
 //import toast, { Toaster } from "react-hot-toast";
 
 import Carousel from "react-multi-carousel";
 import "react-multi-carousel/lib/styles.css";
 import bigO from "../../styles/bigO.webp";
-import {responsive} from '../../helpers/utility' 
+import { responsive } from "../../helpers/utility";
 
 const Pizza = () => {
-    const { menuList } = useContext(ShopContext);
-    const [CategoryPizza, setCategoryPizza] = useState("All");
-    const [windowDimensions, setWindowDimensions] = useState({
+  const { menuList } = useContext(ShopContext);
+  const [CategoryPizza, setCategoryPizza] = useState("All");
+  const [windowDimensions, setWindowDimensions] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight,
+  });
+
+  const FillterCategoryPizza =
+    CategoryPizza === "All"
+      ? menuList.filter(
+          (item) => item.category === "Pizza" || item.category === "Calzone"
+        )
+      : menuList.filter((item) => item.category === CategoryPizza);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowDimensions({
         width: window.innerWidth,
         height: window.innerHeight,
       });
-    
+    };
 
-  const FillterCategoryPizza =
-  CategoryPizza === "All"
-    ? menuList.filter(
-        (item) => item.category === "Pizza" || item.category === "Calzone"
-      )
-    : menuList.filter((item) => item.category === CategoryPizza);
-
-
-      useEffect(() => {
-        const handleResize = () => {
-          setWindowDimensions({
-            width: window.innerWidth,
-            height: window.innerHeight,
-          });
-        };
-    
-        window.addEventListener("resize", handleResize);
-        return () => {
-          window.removeEventListener("resize", handleResize);
-        };
-      }, []);
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
 
   return (
     <div>
-  <div className="filter_container">
-          <div className="filterBTN">
-            <div className="container_Logo">
-              <img src={bigO} className="bigO_logo" alt="" />
-              <h1 className="category_title">Pizza</h1>
-            </div>
-           <div className="RemoveOption">
+      <div className="filter_container">
+        <div className="filterBTN">
+          <div className="container_Logo">
+            <img src={bigO} className="bigO_logo" alt="" />
+            <h1 className="category_title">Pizza</h1>
+          </div>
+          <div className="RemoveOption">
             <div
               className={CategoryPizza === "All" ? "active" : "all"}
               onClick={() => setCategoryPizza("All")}
@@ -70,64 +67,62 @@ const Pizza = () => {
             >
               Calzone
             </div>
-            </div>
           </div>
-          <div className="spreMeniu">
-             <MdOutlineSubdirectoryArrowRight color="white" size={20}  />
-          <Link to="/menu/pizza" className="linkPizza"> 
-            <p >spre meniu Pizza</p>
-          </Link>
-          </div>
-          
         </div>
+        <div className="spreMeniu">
+          <MdOutlineSubdirectoryArrowRight color="white" size={20} />
+          <Link to="/menu/pizza" className="linkPizza">
+            <p>spre meniu Pizza</p>
+          </Link>
+        </div>
+      </div>
 
-        {windowDimensions.width > 767 ? (
-          <div className="caru">
-            <Carousel
-              responsive={responsive}
-              // autoPlay={true}
-              swipeable={true}
-              draggable={true}
-              showDots={false}
-              infinite={true}
-              partialVisible={false}
-              dotListClass="custom-dot-list-style"
-            >
-              {FillterCategoryPizza.map((item, key) => {
-                return (
-                  <MenuItems
-                    //notify={notify}
-                    id={item.id}
-                    key={key}
-                    image={item.image}
-                    name={item.name}
-                    price={item.price}
-                    ingredients={item.ingredients}
-                  />
-                );
-              })}
-            </Carousel>
-          </div>
-        ) : (
-          <div className="menuList">
+      {windowDimensions.width > 767 ? (
+        <div className="caru">
+          <Carousel
+            responsive={responsive}
+            // autoPlay={true}
+            swipeable={true}
+            draggable={true}
+            showDots={false}
+            infinite={true}
+            partialVisible={false}
+            dotListClass="custom-dot-list-style"
+          >
             {FillterCategoryPizza.map((item, key) => {
               return (
                 <MenuItems
+                  //notify={notify}
                   id={item.id}
                   key={key}
                   image={item.image}
                   name={item.name}
                   price={item.price}
                   ingredients={item.ingredients}
-                  //notify={notify}
                 />
               );
             })}
-          </div>
-        )}
-      
+          </Carousel>
+        </div>
+      ) : (
+        <div className="menuList">
+          {FillterCategoryPizza.map((item, key) => {
+            return (
+              <MenuItems
+                id={item.id}
+                key={key}
+                image={item.image}
+                name={item.name}
+                price={item.price}
+                ingredients={item.ingredients}
+                //notify={notify}
+              />
+            );
+          })}
+        </div>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Pizza
+export default Pizza;
