d30d09e917e1ca359c86ab36be182097a94ad856
“armando3069” <jorapula2@gmail.com>
2024-08-15 03:21:57 +0300

    refactor: add try and catch

--- clients/src/helpers/menuList.js
+++ clients/src/helpers/menuList.js
@@ -1,6 +1,11 @@
 import axios from 'axios';
 
 export const fetchMenuList = async () => {
-  const response = await axios.get('http://localhost:3001/menu');
-  return response.data;
+  try {
+    const response = await axios.get('http://localhost:3001/menu');
+    return response.data;
+  } catch (error) {
+    console.error('Error fetching menu list:', error);
+    throw error;  
+  }
 };
