ed9ae17d63d7130458f90d3987ed522a5db04516
armando3069 <ionburueanu1978@gmail.com>
2025-01-05 21:10:30 +0200

    perf: add store redux

--- clients/src/main.jsx
+++ clients/src/main.jsx
@@ -2,18 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { BrowserRouter } from "react-router-dom";
-import Footer from './components/footer';
-
+import Footer from "./components/footer";
+import { Provider } from "react-redux";
+import store, { persistor } from "./redux/store";
+import { PersistGate } from "redux-persist/integration/react";
 
 // Create a root and render the App component within BrowserRouter
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
-    <div className="main-content">
-        <App />
-      </div>
-      <Footer />
-    </BrowserRouter>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <BrowserRouter>
+          <div className="main-content">
+            <App />
+          </div>
+          <Footer />
+        </BrowserRouter>
+      </PersistGate>
+    </Provider>
   </React.StrictMode>
 );
